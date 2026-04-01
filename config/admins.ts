d207fd931f72b68415285c82072917cfb2a2727e
Huajie Zhong <hz642@cornell.edu>
2026-04-01 00:02:26 -0400

    add admin

--- config/admins.ts
+++ config/admins.ts
@@ -1,2 +1,22 @@
-export const ADMIN_EMAILS = ["hz642@cornell.edu"];
+export const ADMIN_EMAILS = [
+    'cornell.perfectmatch@gmail.com',
+    'perfectmatch@cornell.edu',
+    'ps2245@cornell.edu',
+    'yj472@cornell.edu',
+    'njv27@cornell.edu',
+    'dc863@cornell.edu',
+    'vg245@cornell.edu',
+    'sls537@cornell.edu',
+    'kh635@cornell.edu',
+    'hz642@cornell.edu',
+    'js3696@cornell.edu',
+    'az468@cornell.edu',
+    'cz467@cornell.edu',
+    'cjh353@cornell.edu',
+    'cl2674@cornell.edu',
+    'ey272@cornell.edu',
+    'ew537@cornell.edu',
+    'ar2558@cornell.edu',
+];
+
 // Edit this file and redeploy to add/remove admins
